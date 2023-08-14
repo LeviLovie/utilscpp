2571fcc92cc1b7b70f222ca775a482925b1fe6c1
leviiloviee <107021730+LeviiLovie@users.noreply.github.com>
2023-08-14 12:39:18 +0700

    Create logger.hpp

---
+++ logger.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <iostream>
+
+namespace logger {
+    const int LEVEL_DEBUG = 0;
+    const int LEVEL_INFO = 1;
+    const int LEVEL_WARN = 2;
+    const int LEVEL_ERROR = 3;
+    const int LEVEL_FATA = 4;
+    const std::string LEVELS[5] = {"\x1b[36mDEBUG\x1b[0m", "\x1b[32mINFO\x1b[0m", "\x1b[33mWARN\x1b[0m", "\x1b[31mERROR\x1b[0m", "\x1b[31mFATAL\x1b[0m"};
+
+    std::string align_right(std::string str, int width) {
+        int str_len = str.length();
+        if (str_len >= width) {
+            return str;
+        }
+        int spaces = width - str_len;
+        std::string result = "";
+        for (int i = 0; i < spaces; i++) {
+            result += " ";
+        }
+        result += str;
+        return result;
+    }
+
+    void Log(int level_index, std::string message) {
+        std::cout << "\x1b[1m" << align_right(LEVELS[level_index], 21) << "\x1b[0m" << " " << message << std::endl;
+    }
+}
